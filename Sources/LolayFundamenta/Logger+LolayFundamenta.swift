d80f27560ca5ad7e2ea9525a7fee423baec801bc
Gary Rudolph <GaryRudolph@mac.com>
2023-01-26 16:42:35 -0800

    Added equivalent logForType for Logger similar to OSLog

---
+++ Sources/LolayFundamenta/Logger+LolayFundamenta.swift
@@ -0,0 +1,35 @@
+//
+//  Copyright © 2023 Lolay, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import os.log
+
+public extension Logger {
+    /*
+     Creates a log using the class type as the category and the bundle identifer as the subsystem
+     - parameter type: The class type to use for the log. This will turn the type into a string.
+     - parameter bundle: The bundle to use for the subsystem name
+     */
+    static func logForType<T>(_ type: T.Type, bundle: Bundle = Bundle.main) -> Logger {
+        let subsystem: String = {
+            if let bundleIdentifier = bundle.bundleIdentifier {
+                return bundleIdentifier
+            }
+            return bundle.bundlePath
+        }()
+        return Logger(subsystem: subsystem, category: String(describing: type))
+    }
+}
